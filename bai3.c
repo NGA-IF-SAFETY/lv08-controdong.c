5ea4babadf5697f7ee83dd54098d24b0d80a0ba6
NGA-IF-SAFETY <ngatruong01021998@gmail.com>
2016-11-18 09:40:01 +0700

    Create bai3.c

---
+++ bai3.c
@@ -0,0 +1,66 @@
+
+#include <stdio.h>
+
+#include <stdlib.h>
+
+#include<conio.h>
+
+void main()
+
+{
+
+    int i,j,n,t;
+
+    printf("Nhap so phan tu:");
+
+    scanf("%d",&n);
+
+    int *p = (int *)malloc(n*sizeof(int));
+
+    for(i=0;i<n;i++)
+
+    {
+
+        printf("\nNhap gia tri phan tu thu %d:",i);
+
+        scanf("%d",p+i);
+
+    }
+
+    for(i=0;i<n;i++)
+
+    {
+
+        t = *(p+i);
+
+        for(j=i+1;j<n;j++)
+
+        {
+
+            if(t > *(p+j))
+
+            {
+
+                *(p+i) = *(p+j);
+
+                *(p+j) = t;
+
+                t = *(p+i);
+
+            }
+
+        }
+
+    }
+
+    printf("\nDay da duoc sap xep la:");
+
+    for(i=0;i<n;i++)
+
+    {
+
+        printf("%6d  ",*(p+i));
+
+    }
+
+}
