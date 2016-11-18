75f4c3c76049ca3c2e196a962caefc8020fb150b
NGA-IF-SAFETY <ngatruong01021998@gmail.com>
2016-11-18 10:42:37 +0700

    Create bai8.c

---
+++ bai8.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+void main() 
+
+{
+
+int n;
+
+double s,i;
+
+printf("nhap n vao : ");
+
+scanf("%d",&n);
+
+for (i=1;i<=n;i++)
+
+    {
+
+        s+=1/i;
+
+    }
+  printf("\n tong cua day n la:%f",s);
+ 
+ }
